adddf30e96bef3f2886a53f942180bde599d4256
201steve <jeongillee201@gmail.com>
2024-01-27 00:54:03 +0900

    Revert "darkmode"
    This reverts commit 89045725b95544c1ad6e0064c6f99402541e2e84.

--- VIRTUAL_KEYBOARD/js/index.js
+++ VIRTUAL_KEYBOARD/js/index.js
@@ -1,19 +1,2 @@
-class Keyboard {
-    #switchEl;
-    constructor() {
-        this.#assignElement()
-        this.#addEvent()
-
-    }
-    #assignElement(){
-        this.#switchEl = document.getElementById("switch")
-    }
-
-    #addEvent(){
-        this.#switchEl.addEventListener("change",(event)=>{
-            document.documentElement.setAttribute("theme",event.target.checked ? "dark-mode":"")
-
-        })
-    }
-}
-new Keyboard()
+console.log("helloworld")
+console.log("something wrong")
