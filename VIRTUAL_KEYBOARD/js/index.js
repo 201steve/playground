28b925266825257aeba17526c0734c710f276ee4
201steve <jeongillee201@gmail.com>
2024-01-27 13:47:08 +0900

    keyboardEvent 추가

--- VIRTUAL_KEYBOARD/js/index.js
+++ VIRTUAL_KEYBOARD/js/index.js
@@ -1,7 +1,10 @@
 class Keyboard {
     #switchEl;
     #fontSelectEl;
-    #containerEl
+    #containerEl;
+    #keyboardEl;
+    #inputGroupEl;
+    #inputEl;
     constructor() {
         this.#assignElement()
         this.#addEvent()
@@ -9,13 +12,23 @@ class Keyboard {
     }
     #assignElement(){
         this.#containerEl = document.getElementById("container")
+        //getElementById는 document에서만 찾을 수 있다. element단위로는 찾을 수 없다.
         this.#switchEl = this.#containerEl.querySelector("#switch")
         this.#fontSelectEl = this.#containerEl.querySelector("#font")
+        this.#keyboardEl = this.#containerEl.querySelector("#keyboard")
+        this.#inputGroupEl =this.#containerEl.querySelector("#input-group")
+        this.#inputEl =this.#inputGroupEl.querySelector("#input")
     }
 
     #addEvent(){
         this.#switchEl.addEventListener("change",this.#onChangeTheme)
         this.#fontSelectEl.addEventListener("change",this.#onChangeFont)
+        document.addEventListener("keydown",this.#onKeyDown.bind(this))
+        document.addEventListener("keyup",this.#onKeyUp.bind(this))
+        this.#inputEl.addEventListener("input",(event)=>{
+            this.#inputEl.value = this.#inputEl.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/,"")
+            }
+        )
     }
     #onChangeTheme(event){
         document.documentElement.setAttribute("theme",event.target.checked ? "dark-mode":"")
@@ -23,5 +36,16 @@ class Keyboard {
     #onChangeFont(event){
         document.body.style.fontFamily = event.target.value
     }
+
+    #onKeyDown(event){
+        this.#inputGroupEl.classList.toggle("error",/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key))
+        this.#keyboardEl.querySelector(`[data-code=${event.code}]`)?.classList.add("active")
+    }
+
+    #onKeyUp(event){
+        this.#keyboardEl.querySelector(`[data-code=${event.code}]`)?.classList.remove("active")
+    }
+
+
 }
 new Keyboard()
